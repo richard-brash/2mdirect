a0184fb2dab3a544dd4e001e198cee1b8e7d2265
denkovski <denkovski_ii@yahoo.com>
2015-03-11 22:26:07 +0100

    Become familiar with the 2mdirect project fixes #2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var response = {
+        success: true,
+        data: 'You are running NodeJS, this is the 2MDirect Project. I HAVE UDAPTED THE CODE.',
+        error: null
+    }; 
+  res.render('index', { title: response.data, error: response.error });
 });
 
 module.exports = router;
